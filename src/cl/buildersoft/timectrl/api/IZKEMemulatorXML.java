efbc719344fc01dd97d3fa3e8a1fb31c9b863ad1
Claudio Moscoso <claudio.moscoso@gmail.com>
2016-01-14 18:41:45 -0300

    Emulacion de relojes control

--- src/cl/buildersoft/timectrl/api/IZKEMemulatorXML.java
+++ src/cl/buildersoft/timectrl/api/IZKEMemulatorXML.java
@@ -1,41 +1,115 @@
 package cl.buildersoft.timectrl.api;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+
+import cl.buildersoft.framework.exception.BSConfigurationException;
+import cl.buildersoft.framework.util.BSConfig;
+
 import com4j.Com4jObject;
 import com4j.ComThread;
 import com4j.EventCookie;
 import com4j.Holder;
 
-public class IZKEMemulator implements _zkemProxy {
-	public int maxRecords = 4;
-	private Integer counter = 0;
+public class IZKEMemulatorXML implements _zkemProxy {
+	private static final String FILENAME = "MachinesEmu.xml";
+	private String filePathEmu = null;
+	Element rootElement = null;
+	Integer errorNumber = null;
+	String errorDescr = null;
+
+	Element attendances = null;
+	Element employess = null;
+	Integer currentAttendance = 0;
+	Integer currentEmployee = 0;
+
+	public IZKEMemulatorXML() {
+		BSConfig config = new BSConfig();
+		this.filePathEmu = config.fixPath(System.getenv("BS_PATH")) + FILENAME;
+
+	}
+
+	@Override
+	public boolean connect_Net(String ip, short port) {
+		SAXReader reader = new SAXReader();
+		Boolean out = true;
+		try {
+			Document document = reader.read(this.filePathEmu);
+			rootElement = (Element) document.selectSingleNode("/Machines/Machine[@IP='" + ip + "'][@Port='" + port + "']");
+			if (rootElement == null) {
+				out = false;
+				errorNumber = 102;
+				errorDescr = "Machine not found.";
+			}
+		} catch (DocumentException e) {
+			errorNumber = 101;
+			errorDescr = e.getMessage();
+			out = false;
+		}
+
+		return out;
+	}
+
+	@Override
+	public void disconnect() {
+		this.rootElement.getDocument().clearContent();
+	}
 
 	@Override
 	public void getLastError(Holder<Integer> dwErrorCode) {
-		dwErrorCode.value = -100;
+		dwErrorCode.value = this.errorNumber;
 	}
 
 	@Override
 	public boolean readAllUserID(int dwMachineNumber) {
-		this.counter = 0;
+		this.employess = this.rootElement.element("Employees");
 		return true;
 	}
 
 	@Override
 	public boolean getSerialNumber(int dwMachineNumber, Holder<String> dwSerialNumber) {
-		Integer valueInteger = 0;
-		String key = "dwSerialNumber";
+		String serieValue = null;
 
-		String valueString = System.getProperty(key);
-		if (valueString == null) {
-			valueInteger = 101;
-		} else {
-			valueInteger = Integer.parseInt(valueString);
-			valueInteger++;
+		Attribute serie = this.rootElement.attribute("Serie");
+		if (serie == null) {
+			serie = DocumentHelper.createAttribute(this.rootElement, "Serie", "");
 		}
-		valueString = "" + valueInteger;
-		System.setProperty(key, valueString);
-		dwSerialNumber.value = valueString;
+		if (serie.getValue().trim().length() == 0) {
+			serieValue = getRandom();
+			serie.setValue(serieValue);
+			saveDocument();
+		} else {
+			serieValue = serie.getValue();
+		}
+
+		dwSerialNumber.value = serieValue;
 		return true;
+	}
+
+	private void saveDocument() {
+		XMLWriter writer;
+		try {
+			writer = new XMLWriter(new FileWriter(filePathEmu));
+			writer.write(this.rootElement.getDocument());
+			writer.close();
+		} catch (IOException e) {
+			throw new BSConfigurationException(e);
+		}
+	}
+
+	private String getRandom() {
+		SecureRandom secureRandom = new SecureRandom();
+		return new BigInteger(130, secureRandom).toString(32);
 	}
 
 	@Override
@@ -45,7 +119,7 @@ public class IZKEMemulator implements _zkemProxy {
 
 	@Override
 	public boolean beginBatchUpdate(int dwMachineNumber, int updateFlag) {
-		this.counter = 0;
+
 		return true;
 	}
 
@@ -59,39 +133,33 @@ public class IZKEMemulator implements _zkemProxy {
 			Holder<Integer> dwInOutMode, Holder<Integer> dwYear, Holder<Integer> dwMonth, Holder<Integer> dwDay,
 			Holder<Integer> dwHour, Holder<Integer> dwMinute, Holder<Integer> dwSecond, Holder<Integer> dwWorkCode) {
 
-		this.counter++;
-
-		dwEnrollNumber.value = "77" + counter;
-		dwVerifyMode.value = 1;
-		dwInOutMode.value = 1;
-		dwYear.value = 2020;
-		dwMonth.value = 7;
-		dwDay.value = 27;
-		dwHour.value = 23;
-		dwMinute.value = 38;
-		dwSecond.value = 19;
-		dwWorkCode.value = 1;
-
-		if (counter == 101) {
-			// dwDay.value = 32;
+		Element attendance = (Element) this.attendances.selectSingleNode("Attendance[" + (++currentAttendance) + "]");
+		if (attendance != null) {
+			dwEnrollNumber.value = attendance.attributeValue("EnrollNumber");
+			dwVerifyMode.value = Integer.parseInt(attendance.attributeValue("VerifyMode"));
+			dwInOutMode.value = Integer.parseInt(attendance.attributeValue("InOutMode"));
+			dwYear.value = Integer.parseInt(attendance.attributeValue("Year"));
+			dwMonth.value = Integer.parseInt(attendance.attributeValue("Month"));
+			dwDay.value = Integer.parseInt(attendance.attributeValue("Day"));
+			dwHour.value = Integer.parseInt(attendance.attributeValue("Hour"));
+			dwMinute.value = Integer.parseInt(attendance.attributeValue("Minute"));
+			dwSecond.value = Integer.parseInt(attendance.attributeValue("Second"));
+			dwWorkCode.value = Integer.parseInt(attendance.attributeValue("WorkCode"));
 		}
-
-		return counter <= maxRecords;
+		return attendance != null;
 	}
 
 	@Override
 	public boolean ssR_GetAllUserInfo(int dwMachineNumber, Holder<String> dwEnrollNumber, Holder<String> name,
 			Holder<String> password, Holder<Integer> privilege, Holder<Boolean> enabled) {
-		this.counter++;
 
-		dwEnrollNumber.value = dwEnrollNumber.value != null && dwEnrollNumber.value.length() > 0 ? dwEnrollNumber.value : "13"
-				+ this.counter;
-		name.value = "Juan Perez " + this.counter;
+		dwEnrollNumber.value = dwEnrollNumber.value != null && dwEnrollNumber.value.length() > 0 ? dwEnrollNumber.value : "13";
+		name.value = "Juan Perez ";
 		password.value = "xxxxxxxxxxxx";
 		privilege.value = 1;
 		enabled.value = true;
 
-		return this.counter <= maxRecords;
+		return true;
 	}
 
 	@Override
@@ -116,8 +184,8 @@ public class IZKEMemulator implements _zkemProxy {
 	public boolean ssR_GetUserInfo(int machineNumber, Holder<String> enrollNumber, Holder<String> name, Holder<String> password,
 			Holder<Integer> privilege, Holder<Boolean> enabled) {
 
-		enrollNumber.value = "13" + this.counter;
-		name.value = "Juan Perez " + this.counter;
+		enrollNumber.value = "13";
+		name.value = "Juan Perez ";
 		password.value = "xxxxxxxxxxxx";
 		privilege.value = 1;
 		enabled.value = true;
@@ -177,24 +245,13 @@ public class IZKEMemulator implements _zkemProxy {
 	}
 
 	@Override
-	public boolean connect_Net(String ip, short port) {
-		return true;
-	}
-
-	@Override
-	public void disconnect() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	public boolean enableDevice(int machine, boolean enable) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean readGeneralLogData(int machine) {
+		attendances = this.rootElement.element("Attendances");
 		return true;
 	}
 
