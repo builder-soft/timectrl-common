582cb08e5c07c6b67e6dfd69404ba9906aa90e3c
Claudio Moscoso <claudio.moscoso@gmail.com>
2016-07-11 18:24:18 -0400

    Pruebas para hacer la pantalla de consulta de empleado.

--- src/cl/buildersoft/dalea/web/servlet/GetEmployeeInfo.java
+++ src/cl/buildersoft/dalea/web/servlet/GetEmployeeInfo.java
@@ -1,21 +1,33 @@
 package cl.buildersoft.dalea.web.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import cl.buildersoft.framework.web.servlet.BSHttpServlet_;
 
-@WebServlet("/servlet/dalea/web/EmployeeInfo")
+@WebServlet("/servlet/dalea/web/GetEmployeeInfo")
 public class GetEmployeeInfo extends BSHttpServlet_ {
+	private static final Logger LOG = LogManager.getLogger(GetEmployeeInfo.class);
 	private static final long serialVersionUID = -490966795587603364L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LOG.entry();
 
-		forwardOrRedirect(request, response, "/WEB-INF/jsp/employee/employee-info2.jsp");
+		PrintWriter w = response.getWriter();
+		w.print("Hola mundo");
+		w.flush();
+		w.close();
+
+		// forwardOrRedirect(request, response,
+		// "/WEB-INF/jsp/employee/employee-info2.jsp");
+		LOG.exit();
 	}
 
 }
